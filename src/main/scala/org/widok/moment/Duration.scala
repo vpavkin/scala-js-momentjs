3c1ae94221d1aa1c6562666d3a4dd2586d4ac238
Tim Nieradzik <tim@kognit.io>
2015-03-11 15:55:01 +0100

    Make getters/setters available to Date and Duration

--- src/main/scala/org/widok/moment/Duration.scala
+++ src/main/scala/org/widok/moment/Duration.scala
@@ -3,26 +3,10 @@ package org.widok.moment
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSName
 
-trait Duration extends js.Object {
+trait Duration extends js.Object with Getters with Setters[Duration] {
   @JSName("humanize")
   def humanise(): String = js.native
 
   @JSName("humanize")
   def humanise(withSuffix: Boolean): String = js.native
-
-  def milliseconds(): Int = js.native
-  def seconds(): Double = js.native
-  def minutes(): Double = js.native
-  def hours(): Double = js.native
-  def days(): Double = js.native
-  def months(): Double = js.native
-  def years(): Double = js.native
-
-  def add(time: Double, unit: String): Duration = js.native
-  def add(millis: Int): Duration = js.native
-  def add(duration: Duration): Duration = js.native
-
-  def subtract(time: Double, unit: String): Duration = js.native
-  def subtract(millis: Int): Duration = js.native
-  def subtract(duration: Duration): Duration = js.native
 }
---
+++ src/main/scala/org/widok/moment/Getters.scala
@@ -0,0 +1,13 @@
+package org.widok.moment
+
+import scala.scalajs.js
+
+trait Getters extends js.Object {
+  def milliseconds(): Int = js.native
+  def seconds(): Double = js.native
+  def minutes(): Double = js.native
+  def hours(): Double = js.native
+  def days(): Double = js.native
+  def months(): Double = js.native
+  def years(): Double = js.native
+}
