3467bbe9c11d7e02fb90dd00194bad1850216754
Dain Kaplan <dain@tempura.org>
2015-04-02 00:47:09 +0900

    Added a few missing methods like toDate() and add()/subtract() along with unit constants.

--- src/main/scala/org/widok/moment/Date.scala
+++ src/main/scala/org/widok/moment/Date.scala
@@ -2,7 +2,25 @@ package org.widok.moment
 
 import scala.scalajs.js
 
+object Units {
+  val Millisecond = "millisecond"
+  val Second = "second"
+  val Minute = "minute"
+  val Hour = "hour"
+  val Day = "day"
+  val Date = "date"
+  val Week = "week"
+  val ISOWeek = "isoweek"
+  val Month = "month"
+  val Quarter = "quarter"
+  val Year = "year"
+}
+
 trait Date extends js.Object with Getters with Setters[Date] {
+  def toDate(): js.Date = js.native
+  def toISOString(): String = js.native
+  def add(amt: Int, unit: String): Date = js.native
+  def subtract(amt: Int, unit: String): Date = js.native
   def fromNow(): String = js.native
   def fromNow(withoutSuffix: Boolean): String = js.native
   def isDate(): Boolean = js.native
