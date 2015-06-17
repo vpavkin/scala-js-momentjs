9a16a9274904de1a681266a435df04fcabfaa2f9
Tim Nieradzik <tim@kognit.io>
2015-06-17 15:27:33 +0200

    Clean up interface
    See also #2

--- src/main/scala/org/widok/moment/Date.scala
+++ src/main/scala/org/widok/moment/Date.scala
@@ -3,25 +3,9 @@ package org.widok.moment
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSName
 
-object Units {
-  val Millisecond = "millisecond"
-  val Second = "second"
-  val Minute = "minute"
-  val Hour = "hour"
-  val Day = "day"
-  val Date = "date"
-  val Week = "week"
-  val ISOWeek = "isoweek"
-  val Month = "month"
-  val Quarter = "quarter"
-  val Year = "year"
-}
-
 trait Date extends js.Object with Getters with Setters[Date] {
   def toDate(): js.Date = js.native
   def toISOString(): String = js.native
-  def add(amt: Int, unit: String): Date = js.native
-  def subtract(amt: Int, unit: String): Date = js.native
   def fromNow(): String = js.native
   def fromNow(withoutSuffix: Boolean): String = js.native
   def isDate(): Boolean = js.native
@@ -34,7 +18,8 @@ trait Date extends js.Object with Getters with Setters[Date] {
   def isBefore(date: Date, unit: String): Boolean = js.native
   def isAfter(date: Date): Boolean = js.native
   def isAfter(date: Date, unit: String): Boolean = js.native
-  def diff(date: Date, unit: String = Unit.Millisecond): Duration = js.native
+  def diff(date: Date): Duration = js.native
+  def diff(date: Date, unit: String): Duration = js.native
 
   @JSName("valueOf")
   def value(): Double = js.native
--- src/main/scala/org/widok/moment/Setters.scala
+++ src/main/scala/org/widok/moment/Setters.scala
@@ -3,10 +3,12 @@ package org.widok.moment
 import scala.scalajs.js
 
 trait Setters[T] extends js.Object {
+  def add(time: Int, unit: String): T = js.native
   def add(time: Double, unit: String): T = js.native
   def add(millis: Int): T = js.native
   def add(duration: Duration): T = js.native
 
+  def subtract(time: Int, unit: String): T = js.native
   def subtract(time: Double, unit: String): T = js.native
   def subtract(millis: Int): T = js.native
   def subtract(duration: Duration): T = js.native
