18f3131f1df8da771bb56b5bc7d432629834ac93
Michael Ummels <michael@ummels.de>
2015-07-04 15:31:34 +0200

    Add more methods

--- src/main/scala/org/widok/moment/Moment.scala
+++ src/main/scala/org/widok/moment/Moment.scala
@@ -12,6 +12,14 @@ object Moment extends js.Object {
    */
   def apply(millis: Double): Date = js.native
 
+  def apply(moment: Date): Date = js.native
+  def apply(date: js.Date): Date = js.native
+  def apply(string: String): Date = js.native
+  def apply(string: String, format: String): Date = js.native
+  def apply(string: String, format: String, locale: String): Date = js.native
+  def apply(string: String, format: String, strict: Boolean): Date = js.native
+  def apply(string: String, format: String, locale: String, strict: Boolean): Date = js.native
+
   def locale(s: String): Unit = js.native
 
   def duration(millis: Int): Duration = js.native
