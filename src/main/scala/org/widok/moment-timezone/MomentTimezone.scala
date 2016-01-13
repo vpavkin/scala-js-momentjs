19624f382e305c2286f0ae351d308dad158711ec
Matt Oliver <mjo2089@gmail.com>
2016-01-12 21:20:58 -0500

    changed some things around MomentTimezone

--- src/main/scala/org/widok/moment-timezone/MomentTimezone.scala
+++ src/main/scala/org/widok/moment-timezone/MomentTimezone.scala
@@ -7,11 +7,18 @@ import org.widok.moment._
 @js.native
 @JSName("moment-timezone")
 trait MomentTimezone extends js.Object {
-  def apply(date: Double, tz: String): Date = js.native
-  def apply(date: String, format: String, tz: String): Date = js.native
+  def apply(tz: String): MTDate = js.native
+  def apply(date: Double, tz: String): MTDate = js.native
+  def apply(date: String, tz: String): MTDate = js.native
 
   def zone(tz: String): MomentZone = js.native
   def names(): js.Array[String] = js.native
 
   def guess(): String = js.native
+}
+
+@js.native
+trait MTDate extends js.Object {
+  def format(format: String): String = js.native
+  def format(): String = js.native
 }
